8103d58fba0a9006fc75328ba2f14afbaf24a3aa
wimbrees <wimbrees@users.noreply.github.com>
2018-09-07 13:34:33 +0200

    Changed trusted proxies

--- config/database.php
+++ config/database.php
@@ -3,32 +3,32 @@
 return [
 
     /*
-                                            |--------------------------------------------------------------------------
-                                            | Default Database Connection Name
-                                            |--------------------------------------------------------------------------
-                                            |
-                                            | Here you may specify which of the database connections below you wish
-                                            | to use as your default connection for all database work. Of course
-                                            | you may use many connections at once using the Database library.
-                                            |
+                                                    |--------------------------------------------------------------------------
+                                                    | Default Database Connection Name
+                                                    |--------------------------------------------------------------------------
+                                                    |
+                                                    | Here you may specify which of the database connections below you wish
+                                                    | to use as your default connection for all database work. Of course
+                                                    | you may use many connections at once using the Database library.
+                                                    |
     */
 
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
-                                            |--------------------------------------------------------------------------
-                                            | Database Connections
-                                            |--------------------------------------------------------------------------
-                                            |
-                                            | Here are each of the database connections setup for your application.
-                                            | Of course, examples of configuring each database platform that is
-                                            | supported by Laravel is shown below to make development simple.
-                                            |
-                                            |
-                                            | All database work in Laravel is done through the PHP PDO facilities
-                                            | so make sure you have the driver for your particular database of
-                                            | choice installed on your machine before you begin development.
-                                            |
+                                                    |--------------------------------------------------------------------------
+                                                    | Database Connections
+                                                    |--------------------------------------------------------------------------
+                                                    |
+                                                    | Here are each of the database connections setup for your application.
+                                                    | Of course, examples of configuring each database platform that is
+                                                    | supported by Laravel is shown below to make development simple.
+                                                    |
+                                                    |
+                                                    | All database work in Laravel is done through the PHP PDO facilities
+                                                    | so make sure you have the driver for your particular database of
+                                                    | choice installed on your machine before you begin development.
+                                                    |
     */
 
     'connections' => [
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'ec2-54-227-244-12.compute-1.amazonaws.com'),
+            'host' => 'ec2-54-227-244-12.compute-1.amazonaws.com',
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd9p7go47esn1e3'),
-            'username' => env('DB_USERNAME', 'tjdtjlgfraiizp'),
-            'password' => env('DB_PASSWORD', '    afcb43dd83dd51ff84da7851e2fff6014273f2c9ea6f3489b2ba86a93ae2a933'),
+            'database' => 'd9p7go47esn1e3',
+            'username' => 'tjdtjlgfraiizp',
+            'password' => 'afcb43dd83dd51ff84da7851e2fff6014273f2c9ea6f3489b2ba86a93ae2a933',
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
@@ -81,27 +81,27 @@ return [
     ],
 
     /*
-                                            |--------------------------------------------------------------------------
-                                            | Migration Repository Table
-                                            |--------------------------------------------------------------------------
-                                            |
-                                            | This table keeps track of all the migrations that have already run for
-                                            | your application. Using this information, we can determine which of
-                                            | the migrations on disk haven't actually been run in the database.
-                                            |
+                                                    |--------------------------------------------------------------------------
+                                                    | Migration Repository Table
+                                                    |--------------------------------------------------------------------------
+                                                    |
+                                                    | This table keeps track of all the migrations that have already run for
+                                                    | your application. Using this information, we can determine which of
+                                                    | the migrations on disk haven't actually been run in the database.
+                                                    |
     */
 
     'migrations' => 'migrations',
 
     /*
-                                            |--------------------------------------------------------------------------
-                                            | Redis Databases
-                                            |--------------------------------------------------------------------------
-                                            |
-                                            | Redis is an open source, fast, and advanced key-value store that also
-                                            | provides a richer set of commands than a typical key-value systems
-                                            | such as APC or Memcached. Laravel makes it easy to dig right in.
-                                            |
+                                                    |--------------------------------------------------------------------------
+                                                    | Redis Databases
+                                                    |--------------------------------------------------------------------------
+                                                    |
+                                                    | Redis is an open source, fast, and advanced key-value store that also
+                                                    | provides a richer set of commands than a typical key-value systems
+                                                    | such as APC or Memcached. Laravel makes it easy to dig right in.
+                                                    |
     */
 
     'redis' => [
