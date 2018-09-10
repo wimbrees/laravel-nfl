30d11163b92943cef183f1d8894c43174d3a3e3a
wimbrees <wimbrees@users.noreply.github.com>
2018-09-10 19:55:39 +0200

    Removed standings from nav if user not logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,9 +34,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/standings">Standings</a>
-                        </li>
+                        @if (auth()->check())
+                            <li class="nav-item">
+                                <a class="nav-link" href="/standings">Standings</a>
+                            </li>
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
