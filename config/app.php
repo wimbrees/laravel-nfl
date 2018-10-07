6e25d5de0bca19165123e2efbeca3d0d0d552b29
wimbrees <wimbrees@users.noreply.github.com>
2018-10-07 14:08:11 +0200

    Set debug to true

--- config/app.php
+++ config/app.php
@@ -3,105 +3,105 @@
 return [
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Name
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | This value is the name of your application. This value is used when the
-                                                    | framework needs to place the application's name in a notification or
-                                                    | any other location as required by the application or its packages.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Name
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | This value is the name of your application. This value is used when the
+                                                            | framework needs to place the application's name in a notification or
+                                                            | any other location as required by the application or its packages.
+                                                            |
     */
 
     'name' => env('APP_NAME', 'NFL'),
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Environment
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | This value determines the "environment" your application is currently
-                                                    | running in. This may determine how you prefer to configure various
-                                                    | services your application utilizes. Set this in your ".env" file.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Environment
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | This value determines the "environment" your application is currently
+                                                            | running in. This may determine how you prefer to configure various
+                                                            | services your application utilizes. Set this in your ".env" file.
+                                                            |
     */
 
     'env' => env('APP_ENV', 'production'),
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Debug Mode
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | When your application is in debug mode, detailed error messages with
-                                                    | stack traces will be shown on every error that occurs within your
-                                                    | application. If disabled, a simple generic error page is shown.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Debug Mode
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | When your application is in debug mode, detailed error messages with
+                                                            | stack traces will be shown on every error that occurs within your
+                                                            | application. If disabled, a simple generic error page is shown.
+                                                            |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application URL
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | This URL is used by the console to properly generate URLs when using
-                                                    | the Artisan command line tool. You should set this to the root of
-                                                    | your application so that it is used when running Artisan tasks.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application URL
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | This URL is used by the console to properly generate URLs when using
+                                                            | the Artisan command line tool. You should set this to the root of
+                                                            | your application so that it is used when running Artisan tasks.
+                                                            |
     */
 
     'url' => env('APP_URL', 'http://localhost'),
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Timezone
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | Here you may specify the default timezone for your application, which
-                                                    | will be used by the PHP date and date-time functions. We have gone
-                                                    | ahead and set this to a sensible default for you out of the box.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Timezone
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | Here you may specify the default timezone for your application, which
+                                                            | will be used by the PHP date and date-time functions. We have gone
+                                                            | ahead and set this to a sensible default for you out of the box.
+                                                            |
     */
 
     'timezone' => 'Europe/Madrid',
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Locale Configuration
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | The application locale determines the default locale that will be used
-                                                    | by the translation service provider. You are free to set this value
-                                                    | to any of the locales which will be supported by the application.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Locale Configuration
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | The application locale determines the default locale that will be used
+                                                            | by the translation service provider. You are free to set this value
+                                                            | to any of the locales which will be supported by the application.
+                                                            |
     */
 
     'locale' => 'en',
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Application Fallback Locale
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | The fallback locale determines the locale to use when the current one
-                                                    | is not available. You may change the value to correspond to any of
-                                                    | the language folders that are provided through your application.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Application Fallback Locale
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | The fallback locale determines the locale to use when the current one
+                                                            | is not available. You may change the value to correspond to any of
+                                                            | the language folders that are provided through your application.
+                                                            |
     */
 
     'fallback_locale' => 'en',
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Encryption Key
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | This key is used by the Illuminate encrypter service and should be set
-                                                    | to a random, 32 character string, otherwise these encrypted strings
-                                                    | will not be safe. Please do this before deploying an application!
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Encryption Key
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | This key is used by the Illuminate encrypter service and should be set
+                                                            | to a random, 32 character string, otherwise these encrypted strings
+                                                            | will not be safe. Please do this before deploying an application!
+                                                            |
     */
 
     'key' => env('APP_KEY'),
@@ -109,20 +109,20 @@ return [
     'cipher' => 'AES-256-CBC',
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Autoloaded Service Providers
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | The service providers listed here will be automatically loaded on the
-                                                    | request to your application. Feel free to add your own services to
-                                                    | this array to grant expanded functionality to your applications.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Autoloaded Service Providers
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | The service providers listed here will be automatically loaded on the
+                                                            | request to your application. Feel free to add your own services to
+                                                            | this array to grant expanded functionality to your applications.
+                                                            |
     */
 
     'providers' => [
 
         /*
-                                                                                 * Laravel Framework Service Providers...
+                                                                                             * Laravel Framework Service Providers...
         */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
@@ -148,11 +148,11 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-                                                                                 * Package Service Providers...
+                                                                                             * Package Service Providers...
         */
 
         /*
-                                                                                 * Application Service Providers...
+                                                                                             * Application Service Providers...
         */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
@@ -163,14 +163,14 @@ return [
     ],
 
     /*
-                                                    |--------------------------------------------------------------------------
-                                                    | Class Aliases
-                                                    |--------------------------------------------------------------------------
-                                                    |
-                                                    | This array of class aliases will be registered when this application
-                                                    | is started. However, feel free to register as many as you wish as
-                                                    | the aliases are "lazy" loaded so they don't hinder performance.
-                                                    |
+                                                            |--------------------------------------------------------------------------
+                                                            | Class Aliases
+                                                            |--------------------------------------------------------------------------
+                                                            |
+                                                            | This array of class aliases will be registered when this application
+                                                            | is started. However, feel free to register as many as you wish as
+                                                            | the aliases are "lazy" loaded so they don't hinder performance.
+                                                            |
     */
 
     'aliases' => [
